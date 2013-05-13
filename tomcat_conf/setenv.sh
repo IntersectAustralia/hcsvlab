0b107da439b1cc9c831a1bc319d8f5b94817122b
Chris Kenward <chrisk@intersect.org.au>
2013-05-14 08:54:06 +1000

    HCSVLAB-296: Use CATALINA_OPTS to enable remote jmx connections.

--- tomcat_conf/setenv.sh
+++ tomcat_conf/setenv.sh
@@ -4,9 +4,13 @@ JAVA_OPTS="$JAVA_OPTS -DFEDORA_HOME=$FEDORA_HOME -DSOLR_HOME=$SOLR_HOME"
 # Bump up heap and permgen
 JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx512m -XX:MaxPermSize=128m  -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled"
 
-# Enable remote jmx connections for profiling
-#JAVA_OPTS="$JAVA_OPTS  -Dcom.sun.management.jmxremote.port=9000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
-
-export JAVA_HOME
+export JAVA_OPTS
 
 echo "Using JAVA_OPTS: $JAVA_OPTS"
+
+# Enable remote jmx connections for profiling
+CATALINA_OPTS="$CATALINA_OPTS  -Dcom.sun.management.jmxremote.port=9000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+
+export CATALINA_OPTS
+
+echo "Using CATALINA_OPTS= $CATALINA_OPTS"
