d0a1c9ed3c793262d291d39d09c4178def3b0616
Chris Kenward <chrisk@intersect.org.au>
2013-05-14 08:43:16 +1000

    HCSVLAB-296: Increase heap and permgen in JAVA_OPTS.

--- tomcat_conf/setenv.sh
+++ tomcat_conf/setenv.sh
@@ -1,4 +1,12 @@
+# Set FEDORA_HOME and SOLR_HOME so the tomcat config can access them
 JAVA_OPTS="$JAVA_OPTS -DFEDORA_HOME=$FEDORA_HOME -DSOLR_HOME=$SOLR_HOME"
+
+# Bump up heap and permgen
+JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx512m -XX:MaxPermSize=128m  -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled"
+
+# Enable remote jmx connections for profiling
+#JAVA_OPTS="$JAVA_OPTS  -Dcom.sun.management.jmxremote.port=9000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+
 export JAVA_HOME
 
 echo "Using JAVA_OPTS: $JAVA_OPTS"
