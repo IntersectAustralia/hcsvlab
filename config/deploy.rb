5f86ceafa7bcb1d58779bb5a451cfb6f3298d188
elysewise2076 <elyse@intersect.org.au>
2013-05-26 20:54:26 -0700

    fix determination of version path in cap deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -114,7 +114,7 @@ namespace :deploy do
 
   desc "Write the tag that was deployed to a file on the server so we can display it on the app"
   task :write_tag do
-    put branch, "#{release_path}/app/views/shared/_tag.html.haml"
+    put branch, "#{current_path}/app/views/shared/_tag.html.haml"
   end
 
   # Passenger specifics: restart by touching the restart.txt file
