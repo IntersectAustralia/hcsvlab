a062d79c1265518fab9281396298005e9685dd74
Chris Kenward <chrisk@intersect.org.au>
2013-06-04 12:11:18 +1000

    HCSVLAB-245: Fix error in solr schema location when creating solr core

--- config/deploy.rb
+++ config/deploy.rb
@@ -275,7 +275,7 @@ namespace :deploy do
   desc "Create the HCS vLab Solr core"
   task :create_solr_core, :roles => :app do
     run "cp -rp #{current_path}/solr_conf/hcsvlab $SOLR_HOME/", :env => {'RAILS_ENV' => stage}
-    run "cp -p #{current_path}/solr_conf/schema.xml $SOLR_HOME/hcsvlab/solr/hcsvlab-core/conf/schema.xml", :env => {'RAILS_ENV' => stage}
+    run "cp -p #{current_path}/solr_conf/conf/schema.xml $SOLR_HOME/hcsvlab/solr/hcsvlab-core/conf/schema.xml", :env => {'RAILS_ENV' => stage}
   end
 
   desc "Stop the Tomcat 6 server"
