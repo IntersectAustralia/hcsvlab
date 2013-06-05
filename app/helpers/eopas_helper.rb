5a130c331d68f13e71dfca8aa12eea1e9fc0accf
David Clarke <davidc@intersect.org.au>
2013-06-05 12:56:23 +1000

    Added error handler to EOPAS stuff

--- app/helpers/eopas_helper.rb
+++ app/helpers/eopas_helper.rb
@@ -20,13 +20,18 @@ module EopasHelper
     media && transcription
   end
 
-  def is_transcription?(url)
-    uri = URI.parse(url)
-    data = uri.read
-    transcription = detect_format data
+ def is_transcription?(url)
+    # NOTE: Will return false if URL is unavailable
+    begin
+      uri = URI.parse(url)
+      data = uri.read
+      transcription = detect_format data
+    rescue
+      transcription = nil
+    end
     transcription
   end
-
+  
   def detect_format(data)
     result = nil
     flattened = data.downcase
