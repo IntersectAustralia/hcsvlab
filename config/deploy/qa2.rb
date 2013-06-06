8ef1805ad90d04377dc8632c849fd5be64db08bd
Ilya Anisimoff <ilya@intersect.org.au>
2013-06-06 18:08:59 +1000

    MISC: creates parallel qa2 environment

---
+++ config/deploy/qa2.rb
@@ -0,0 +1,7 @@
+# Your HTTP server, Apache/etc
+role :web, 'ic2-hcsvlab-qa2-vm.intersect.org.au'
+# This may be the same as your Web server
+role :app, 'ic2-hcsvlab-qa2-vm.intersect.org.au'
+# This is where Rails migrations will run
+role :db,  'ic2-hcsvlab-qa2-vm.intersect.org.au', :primary => true
+
