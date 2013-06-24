f0062da629aa4596d4f126dd1a7ca5f87f88c70a
Shuqian Hon <sq@intersect.org.au>
2013-06-25 09:49:08 +1000

    HCSVLAB-365: Fix token authentication whitelist logic

--- config/initializers/token_auth.rb
+++ config/initializers/token_auth.rb
@@ -6,9 +6,9 @@ module Devise
       def valid?
         valid = super
         if params[:format] == "json" 
-          valid = valid and ((params[:controller] == 'item_lists' and (params[:action] == 'index' or params[:action] == 'show')) or (params[:controller] == 'catalog' and params[:action] == 'show'))
+          valid = valid && ((params[:controller] == 'item_lists' &&  %w{index show}.include?(params[:action])) || (params[:controller] == 'catalog' &&  %w{primary_text show}.include?(params[:action])))
         else
-          valid = valid and (params[:controller] == 'catalog' and (params[:action] == 'primary_text' or params[:action] == 'document'))
+          valid = valid && (params[:controller] == 'catalog' && params[:action] == 'document')
         end
         valid
       end
