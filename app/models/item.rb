5cad92e8c2d7d64e1fe8611e5cf685bb5250f924
Chris Kenward <chrisk@intersect.org.au>
2013-07-25 09:45:01 +1000

    HCSVLAB-438: Add method to find an Item using its collection name and id

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,4 +17,16 @@ class Item < ActiveFedora::Base
     delegate :collection,    to: 'descMetadata'
     delegate :collection_id, to: 'descMetadata'
 
+    #
+    # Find an item using its collection name and id
+    #
+    def Item.find_by_collection_id(collection, id)
+        results = Item.find_with_conditions('*:*', 
+                                            :fl => 'id', 
+                                            :fq => 'collection_tesim:' + collection.to_s +
+                                                   ' AND collection_id_tesim:' + id.to_s )
+        Rails.logger.warn "Multiple items for collection= #{collection} id= #{id}" if results.count > 1
+        return Item.find(results[0])
+    end
+
 end
