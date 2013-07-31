3d05b3fa1b18be7d82670b88b936850c0e523d24
matthew-intersect <matthew@intersect.org.au>
2013-07-31 17:03:40 +1000

    HCSVLAB-457 Fix up collection model find methods (if they're really needed at all)

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -23,25 +23,15 @@ class Collection < ActiveFedora::Base
   #
   # Find a collection using its uri
   #
-  def Item.find_by_uri(uri)
-    results = Item.find_with_conditions('*:*',
-                                        :fl => 'id',
-                                        :fq => 'uri_tesim:' + uri.to_s )
-    Rails.logger.warn "No collection with URI = #{uri}" if results.count == 0
-    Rails.logger.warn "Multiple collections with URI = #{uri}" if results.count > 1
-    return Item.find(results[0])
+  def Collection.find_by_uri(uri)
+    return Collection.where(uri: uri)
   end
 
   #
   # Find a collection using its short_name
   #
-  def Item.find_by_short_name(short_name)
-    results = Item.find_with_conditions('*:*',
-                                        :fl => 'id',
-                                        :fq => 'short_name_tesim:' + short_name.to_s )
-    Rails.logger.warn "No collection with short name = #{short_name}" if results.count == 0
-    Rails.logger.warn "Multiple collections with short name = #{short_name}" if results.count > 1
-    return Item.find(results[0])
+  def Collection.find_by_short_name(short_name)
+    return Collection.where(short_name: short_name)
   end
 
 end
