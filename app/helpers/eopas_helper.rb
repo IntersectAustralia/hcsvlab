e6f33ed0f9f1f5134a767015c803b4ca4d4ab99b
Chris Kenward <chrisk@intersect.org.au>
2013-08-02 09:08:22 +1000

    HCSVLAB-438: Catch rubydora exception when there is no document content

--- app/helpers/eopas_helper.rb
+++ app/helpers/eopas_helper.rb
@@ -13,15 +13,20 @@ module EopasHelper
     item.documents.each do |document|
       file_name = document.file_name[0]
       next if file_name.nil?
-      if file_name.ends_with? 'xml'
-        transcription = is_transcription? document.datastreams['CONTENT1'].content
-        doc_count += 1
-      else
-        media_format = detect_media_format document.datastreams['CONTENT1'].content
-        if media_format == 'audio' or media_format == 'video'
-          media = true
-          media_count += 1
+      begin
+        doc_content = document.datastreams['CONTENT1'].content
+        if file_name.ends_with? 'xml'
+          transcription = is_transcription? doc_content
+          doc_count += 1
+        else
+          media_format = detect_media_format doc_content
+          if media_format == 'audio' or media_format == 'video'
+            media = true
+            media_count += 1
+          end
         end
+      rescue Exception => msg
+        Rails.logger.warn("Error reading document content: #{msg}")
       end
     end
     Rails.logger.debug("#{id.to_s}: media= #{media} transcription= #{transcription} media_count= #{media_count} doc_count=#{doc_count}")
