7d7403f09cf25211ffb2ad0fe039421b1416a0bf
David Clarke <davidc@intersect.org.au>
2013-08-05 15:56:28 +1000

    HCSVLAB-459: Return the right kind of object from the find methods

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -47,14 +47,14 @@ class Collection < ActiveFedora::Base
   # Find a collection using its uri
   #
   def Collection.find_by_uri(uri)
-    return Collection.where(uri: uri)
+    return Collection.where(uri: uri).all
   end
 
   #
   # Find a collection using its short_name
   #
   def Collection.find_by_short_name(short_name)
-    return Collection.where(short_name: short_name)
+    return Collection.where(short_name: short_name).all
   end
 
 end
