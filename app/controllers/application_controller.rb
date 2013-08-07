0484b7415e0ed779fc57cf2b6569bda55e1dfa68
Gabriel E. Gasser Noblia <gabriel@intersect.org.au>
2013-08-07 12:21:39 +1000

    HCSVLAB-501:  Force browser to no-cache to avoid strange behaviours when back button is pressed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 
   prepend_before_filter :get_api_key
+  #This will force a reload when the back button is pressed.
+  before_filter :set_cache_buster
 
   include ErrorResponseActions
   
@@ -14,6 +16,13 @@ class ApplicationController < ActionController::Base
       params[:api_key] = request.headers["X-API-KEY"]
     end
   end
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+
 
   # Adds a few additional behaviors into the application controller 
    include Blacklight::Controller
