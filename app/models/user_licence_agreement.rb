5a97ab055da20d4599f58675e50af0ea5cdf33f6
David Clarke <davidc@intersect.org.au>
2013-08-22 14:47:11 +1000

    HCSVLAB-291: Support hierarchy of access types

--- app/models/user_licence_agreement.rb
+++ app/models/user_licence_agreement.rb
@@ -5,4 +5,16 @@ class UserLicenceAgreement < ActiveRecord::Base
 
   belongs_to :user
   attr_accessible :groupName, :licenceId
+
+  def self.type_or_higher(access_type)
+    case access_type
+      when DISCOVER_ACCESS_TYPE
+        return [DISCOVER_ACCESS_TYPE, READ_ACCESS_TYPE, EDIT_ACCESS_TYPE]
+      when READ_ACCESS_TYPE
+        return [READ_ACCESS_TYPE, EDIT_ACCESS_TYPE]
+      when EDIT_ACCESS_TYPE
+        return [EDIT_ACCESS_TYPE]
+    end
+  end
+
 end
