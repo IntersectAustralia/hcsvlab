5ea5a8224a57ce45e0bec7dfa8136733d92abf70
matthew-intersect <matthew@intersect.org.au>
2013-09-23 12:38:45 +1000

    HCSVLAB:565 More small fixes to ingest

--- app/models/datastream/item_metadata.rb
+++ app/models/datastream/item_metadata.rb
@@ -2,7 +2,6 @@ class Datastream::ItemMetadata < ActiveFedora::OmDatastream
 
   set_terminology do |t|
     t.root(path: "fields")
-    t.identifier(index_as: :stored_searchable)
     t.handle(index_as: :stored_searchable)
   end
 
