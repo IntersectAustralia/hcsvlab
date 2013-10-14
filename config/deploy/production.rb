954b9d6a7bdcaa69a0ab660529bdd1574d0541b8
David Clarke <davidc@intersect.org.au>
2013-10-14 14:53:42 +1100

    HCSVLAB-625 trying some other combination

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # Your HTTP server, Apache/etc
-role :web, 'ic2-hcsvlab-prod-webapp.intersect.org.au/'
+role :web, 'hcsvlab-prod-webapp.intersect.org.au/'
 # This may be the same as your Web server
-role :app, 'ic2-hcsvlab-prod-webapp.intersect.org.au/'
+role :app, 'hcsvlab-prod-webapp.intersect.org.au/'
 # This is where Rails migrations will run
-role :db,  'ic2-hcsvlab-prod-webapp.intersect.org.au/', :primary => true
+role :db,  'hcsvlab-prod-webapp.intersect.org.au/', :primary => true
