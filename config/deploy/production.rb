f9d0aa6470e0c175d37d3ebc09ed3e662c2b10b3
David Clarke <davidc@intersect.org.au>
2013-10-14 15:00:14 +1100

    Another tweak to see if I can get it going

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # Your HTTP server, Apache/etc
-role :web, 'hcsvlab-prod-webapp.intersect.org.au/'
+role :web, 'hcsvlab-prod-webapp.intersect.org.au'
 # This may be the same as your Web server
-role :app, 'hcsvlab-prod-webapp.intersect.org.au/'
+role :app, 'hcsvlab-prod-webapp.intersect.org.au'
 # This is where Rails migrations will run
-role :db,  'hcsvlab-prod-webapp.intersect.org.au/', :primary => true
+role :db,  'hcsvlab-prod-webapp.intersect.org.au', :primary => true
