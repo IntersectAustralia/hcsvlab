948fdba6cf95914c2346e73805885b40590bfe0f
Gabriel E. Gasser Noblia <gabriel@intersect.org.au>
2013-10-21 14:14:39 +1100

    No-Ticket: Fixed Possible Security Issue.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HcsvlabWeb::Application.config.secret_token = 'a4de2b8b91e6279b3f2b117998e6f9dd5287b41520e78f13a2279d052f9f849d3101594cc893f70c605ee8b87b6d53bfe7a852ae6413d504ce68bdc41a5ecd25'
+HcsvlabWeb::Application.config.secret_token = 'REPLACE_THIS_WITH_REAL_GOOD_RANDOM_STRING'
