40f1f858382f12fd3d341e111238c579466435ef
Gabriel E. Gasser Noblia <gabriel@intersect.org.au>
2013-10-25 12:07:52 +1100

    HCSVLAB-517, HCSVLAB-608: Extracted code for zipping an item list to a helper class

--- app/controllers/item_lists_controller.rb
+++ app/controllers/item_lists_controller.rb
@@ -1,20 +1,16 @@
+require "#{Rails.root}/lib/item/download_items_helper.rb"
+
 class ItemListsController < ApplicationController
   include Blacklight::BlacklightHelperBehavior
   include Blacklight::Configurable
   include Blacklight::SolrHelper
+  include Item::DownloadItemsHelper
 
   before_filter :authenticate_user!
   load_and_authorize_resource
 
   # Set itemList tab as current selected
   set_tab :itemList
-
-  FIXNUM_MAX = 2147483647
-  #
-  # Class variables for information about Solr
-  #
-  @@solr_config = nil
-  @@solr = nil
 
   def index
     if (session[:profiler])
@@ -28,13 +24,17 @@ class ItemListsController < ApplicationController
       @profiler = session[:profiler]
       session.delete(:profiler)
     end
-    @response = @item_list.get_items(params[:page], params[:per_page])
-    @document_list = @response["response"]["docs"]
-    respond_to do |format|
-      format.html { render :index }
-      format.json
+
+    if ('zip' == request.format)
+      download_as_zip
+    else
+      @response = @item_list.get_items(params[:page], params[:per_page])
+      @document_list = @response["response"]["docs"]
+      respond_to do |format|
+        format.html { render :index }
+        format.json
+      end
     end
-    
   end
   
   def create
@@ -175,7 +175,12 @@ class ItemListsController < ApplicationController
       itemsId = @item_list.get_item_ids
 
       # Creates a ZIP file containing the documents and item's metadata
-      zip_path = get_zip_with_documents_and_metadata(itemsId)
+      zip_path = DownloadItemsInZipFormat.new.createAndRetrieveZipPath(itemsId) do |aDoc|
+          @document = aDoc
+          renderer = Rabl::Renderer.new('catalog/show', @document, { :format => 'json', :view_path => 'app/views', :scope => self })
+          itemMetadata = renderer.render
+          itemMetadata
+      end
 
       # Sends the zipped file
       file_name = "#{@item_list.name}.zip"
@@ -208,155 +213,4 @@ class ItemListsController < ApplicationController
     item_list.add_items(documents_ids) unless item_list.nil?
   end
 
-  #
-  # Creates a ZIP file containing all the documents and metadata for the items listed in 'itemsId'.
-  # The returned format respect the BagIt format (http://en.wikipedia.org/wiki/BagIt)
-  #
-  def get_zip_with_documents_and_metadata(itemsId)
-    if (!itemsId.nil? and !itemsId.empty?)
-      begin
-        fileNamesByItem = get_documents_path(itemsId)
-
-        digest_filename = Digest::MD5.hexdigest(itemsId.inspect.to_s)
-        bagit_path = "#{Rails.root.join("tmp", "#{digest_filename}_tmp")}"
-        Dir.mkdir bagit_path
-
-        # make a new bag at base_path
-        bag = BagIt::Bag.new bagit_path
-
-        # add items metadata to the bag
-        add_items_metadata_to_the_bag(fileNamesByItem, bag)
-
-        # add items documents to the bag
-        add_items_documents_to_the_bag(fileNamesByItem, bag)
-
-        # generate the manifest and tagmanifest files
-        bag.manifest!
-
-        zip_path = "#{Rails.root.join("tmp", "#{digest_filename}.tmp")}"
-        zip_file = File.new(zip_path, 'a+')
-        ZipBuilder.build_zip(zip_file, Dir["#{bagit_path}/*"])
-
-        zip_path
-      ensure
-        zip_file.close if !zip_file.nil?
-        FileUtils.rm_rf bagit_path if !bagit_path.nil?
-      end
-    end
-  end
-
-  #
-  # This method will add all the documents listed in 'fileNamesByItem' to the 'bag'
-  #
-  # fileNamesByItem = Hash structure containing the items id as key and the list of files as value
-  #                   Example:
-  #                           {"hcsvlab:1003"=>{handle: "handle1", files:["full_path1, full_path2, .."]} ,
-  #                            "hcsvlab:1034"=>{handle: "handle2", files:["full_path4, full_path5, .."]}}
-  # bag = BagIt::Bag object
-  #
-  def add_items_documents_to_the_bag(fileNamesByItem, bag)
-
-    fileNamesByItem.each_pair do |itemId, info|
-      filenames = info[:files]
-      handle = (info[:handle].nil?)? itemId.gsub(":", "_") : info[:handle]
-
-      filenames.each do |file|
-        title = file.split('/').last
-        # make a new file
-        bag.add_file("#{handle}/#{title}") do |io|
-          io.puts IO.read(file)
-        end
-      end
-    end
-  end
-
-  #
-  # This method will add each item metadata for the items listed in 'fileNamesByItem' to the 'bag'
-  # It will also modify the parameter 'fileNamesByItem' to set the item handle
-  #
-  # fileNamesByItem = Hash structure containing the items id as key and the list of files as value
-  #                   Example:
-  #                           {"hcsvlab:1003"=>{handle: "handle1", files:["full_path1, full_path2, .."]} ,
-  #                            "hcsvlab:1034"=>{handle: "handle2", files:["full_path4, full_path5, .."]}}
-  # bag = BagIt::Bag object
-  #
-  def add_items_metadata_to_the_bag(fileNamesByItem, bag, batch_group = 50)
-    itemsId = fileNamesByItem.keys
-    itemsId.in_groups_of(batch_group, false) do |groupOfItemsId|
-      # create disjunction condition with the items Ids
-      condition = groupOfItemsId.map{|itemId| "id:\"#{itemId.gsub(":", "\:")}\""}.join(" OR ")
-
-      params[:q] = condition
-      params[:rows] = FIXNUM_MAX
-
-      (response, document_list) = get_search_results params
-      document_list.each do |aDoc|
-        handle = aDoc['handle'].gsub(":", "_")
-        itemId = aDoc['id']
-        fileNamesByItem[itemId][:handle] = handle
-
-        @document = aDoc
-        renderer = Rabl::Renderer.new('catalog/show', @document, { :format => 'json', :view_path => 'app/views', :scope => self })
-        itemMetadata = renderer.render
-
-        bag.add_file("#{handle}/#{handle}-metadata.json") do |io|
-          io.puts itemMetadata
-        end
-
-        #puts "### Stat: " + GC.stat.inspect
-
-      end
-    end
-  end
-
-  #
-  # Retrieves the path for the documents which belong to the items listed in 'itemsId'
-  #
-  def get_documents_path(itemsId, batch_group = 50)
-    bench_start = Time.now
-
-    # In this first step we will collect the file location for the documents belonging to the requested items
-    fileNamesByItem = {}
-    itemsId.in_groups_of(batch_group, false) do |groupOfItemsId|
-
-      # create disjunction condition with the items Ids
-      condition = groupOfItemsId.map{|itemId| "item_id_tesim:\"#{itemId.gsub(":", "\:")}\""}.join(" OR ")
-
-      # query SOLR restricting to retrieve only the object profile field
-      objectsProfile = Document.find_with_conditions(condition, {fl:"object_profile_ssm, item_id_tesim", rows:FIXNUM_MAX})
-
-      # Parse every object profile and extract the file location
-      objectsProfile.each do |anObjectProfile|
-        itemId = anObjectProfile["item_id_tesim"].first
-        fileNamesByItem[itemId] = {handle: nil, files:[]} if fileNamesByItem[itemId].nil?
-
-        jsonString = anObjectProfile["object_profile_ssm"].first
-        json = JSON.parse(jsonString)
-
-        filename = json["datastreams"]["CONTENT1"]["dsLocation"]
-        fileNamesByItem[itemId][:files] << filename.to_s.gsub("file://", "")
-      end
-    end
-
-    Rails.logger.debug("Time for retrieve documents path for #{itemsId.length} items: (#{'%.1f' % ((Time.now.to_f - bench_start.to_f)*1000)}ms)")
-
-    return fileNamesByItem
-  end
-
-  #
-  # blacklight uses this method to get the SOLR connection.
-  def blacklight_solr
-    get_solr_connection
-    @@solr
-  end
-
-  #
-  # Initialise the connection to Solr
-  #
-  def get_solr_connection
-    if @@solr_config.nil?
-      @@solr_config = Blacklight.solr_config
-      @@solr        = RSolr.connect(@@solr_config)
-    end
-  end
 end
