38326b661d7a936c3a55277c62188ee2055e38ef
matthew-intersect <matthew@intersect.org.au>
2013-12-05 12:11:22 +1100

    HCSVLAB-664 Update staging2 environment config

--- config/deploy/staging2.rb
+++ config/deploy/staging2.rb
@@ -4,3 +4,7 @@ role :web, 'ic2-hcsvlab-staging2-vm.intersect.org.au'
 role :app, 'ic2-hcsvlab-staging2-vm.intersect.org.au'
 # This is where Rails migrations will run
 role :db,  'ic2-hcsvlab-staging2-vm.intersect.org.au', :primary => true
+
+set :server_url, "http://ic2-hcsvlab-staging2-vm.intersect.org.au"
+set :galaxy_port, "8081"
+set :toolshed_port, "9009"
