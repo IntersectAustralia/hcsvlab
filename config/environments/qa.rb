6b8b3cfc7396ce966e42678a0549c4122165f6d2
Gabriel E. Gasser Noblia <gabriel@intersect.org.au>
2014-03-17 12:32:55 +1100

    HCSVLAB-802: Changed GA tracker id

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -1,7 +1,7 @@
 HcsvlabWeb::Application.configure do
 
   # replace this with your tracker code
-  GA.tracker = "UA-49040235-3"
+  GA.tracker = "UA-49039039-1"
 
   # TODO: set your own correct URL for action mailer
   config.action_mailer.default_url_options = { :host => 'ic2-hcsvlab-test4-vm.intersect.org.au' }
