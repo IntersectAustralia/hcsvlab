68de7ec70da3d3e49c466e7a3a07796a3cc27b4d
Gabriel E. Gasser Noblia <gabriel@intersect.org.au>
2014-03-31 13:44:14 +1100

    HCSVLAB-837: Fixed failing tests.

--- features/step_definitions/item_lists_steps.rb
+++ features/step_definitions/item_lists_steps.rb
@@ -106,7 +106,7 @@ Then /^frequency search for "(.*)" in item list "(.*)" should show error$/ do |t
 end
 
 Then(/^I should get the API config file for "(.*?)"$/) do |user|
-  page.response_headers['Content-Disposition'].should include("filename=\"hcsvlab.config\"")
+  page.response_headers['Content-Disposition'].should include("filename=\"#{PROJECT_PREFIX_NAME}.config\"")
   page.response_headers['Content-Disposition'].should include("attachment")
 
   page.body.should eq('{"base_url":"http://www.example.com/","apiKey":"' + User.find_by_email(user).authentication_token + '","cacheDir":"wrassp_cache"}
