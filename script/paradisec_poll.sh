e2f7993a716cfe935a62e3840eef427764d5ec36
Shuqian Hon <sq@intersect.org.au>
2015-04-29 17:18:51 +1000

    HCSVLAB-918: Remove usage of nohup in cron script.

--- script/paradisec_poll.sh
+++ script/paradisec_poll.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source $HOME/.bash_profile
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [ -z "$RAILS_ENV" ]; then
     RAILS_ENV=development
@@ -31,11 +33,26 @@ else
     pip install "pyparsing<=1.5.7" xlrd "rdflib<=3.4.0" httplib2 rdfextras
 fi
 
+logfile=$DIR/../log/$( date +%Y%m%d_%H.%M.%S )_paradisec_ingest.log
+echo "Logging to $logfile"
+
 rm -r /data/${RAILS_ENV}_raw/paradisec
-bin/python2.7 hcsvlab_robochef/paradisec/harvest/harvester.py /data/${RAILS_ENV}_raw/paradisec >> paradisec_ingest.log
-bin/python2.7 hcsvlab_robochef/paradisec/harvest/collection_harvester.py /data/${RAILS_ENV}_raw/paradisec >> paradisec_ingest.log
-bin/python2.7 main.py paradisec >> paradisec_ingest.log
+bin/python2.7 hcsvlab_robochef/paradisec/harvest/harvester.py /data/${RAILS_ENV}_raw/paradisec >> $logfile 2>&1
+bin/python2.7 hcsvlab_robochef/paradisec/harvest/collection_harvester.py /data/${RAILS_ENV}_raw/paradisec >> $logfile 2>&1
+bin/python2.7 main.py paradisec >> $logfile 2>&1
 
 cd $DIR/..
 bundle exec rake fedora:paradisec_clear
-./ingest_all.sh /data/${RAILS_ENV}_processed/paradisec
+
+declare corpora
+
+for f in `find /data/${RAILS_ENV}_processed/paradisec -type d`
+do
+   if test -n "$(find ${f} -maxdepth 1 -name '*-metadata.rdf' -print -quit)"
+   then
+      echo "... queuing $f for ingest"
+      corpora+=" $f"
+   fi
+done
+
+./nohup_ingest_all_bg.sh ${corpora} >> $logfile 2>&1
