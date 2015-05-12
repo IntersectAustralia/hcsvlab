9f8237e29e6202e76c7d1480dfa8887737d8a587
Shuqian Hon <sq@intersect.org.au>
2015-05-12 15:18:44 +1000

    HCSVLAB-918: Improve setup process.

--- script/paradisec_poll.sh
+++ script/paradisec_poll.sh
@@ -23,19 +23,48 @@ else
     cd $robochef_path
 fi
 
+function setup_error {
+    if [ $1 -ne 0 ]
+    then
+      echo "Error setting up Robochef."
+      exit $1
+    fi
+}
 if [ -f "bin/activate" ]
 then
     source bin/activate
 else
+    sudo chown -R devel:devel /usr/local/
+    which python2.7
+    if [ $? -ne 0 ]
+    then
+        curl -OL http://python.org/ftp/python/2.7.5/Python-2.7.5.tgz
+        tar xvf Python-2.7.5.tgz
+        cd Python-2.7.5
+        ./configure --prefix=/usr/local
+        sudo make && sudo make altinstall
+        sudo chown -R devel:devel /usr/local/lib/python2.7
+        sudo chmod -r a+w /usr/local/lib/python2.7
+
+        setup_error $?
+        cd $robochef_path
+    fi
+
     cp $robochef_path/config.ini.dist $robochef_path/config.ini
     echo "DOCUMENT_BASE_URL = file:///data/production_collections/" >> $robochef_path/config.ini
     echo "CORPUS_BASEDIR = ${raw_path}/" >> $robochef_path/config.ini
     echo "CORPUS_OUTPUTDIR = ${processed_path}/" >> $robochef_path/config.ini
-    curl https://bootstrap.pypa.io/ez_setup.py -o - | sudo python
+
+    curl https://bootstrap.pypa.io/ez_setup.py -o - | python2.7
+    setup_error $?
     easy_install-2.7 virtualenv
-    virtualenv .
+    setup_error $?
+    virtualenv-2.7 .
+    setup_error $?
     source bin/activate
+    setup_error $?
     pip install "pyparsing<=1.5.7" xlrd "rdflib<=3.4.0" httplib2 rdfextras
+    setup_error $?
 fi
 
 logfile=$DIR/../log/paradisec_ingest.log
@@ -45,7 +74,11 @@ rm -r ${raw_path}/paradisec
 bin/python2.7 hcsvlab_robochef/paradisec/harvest/harvester.py ${raw_path}/paradisec >> $logfile 2>&1
 bin/python2.7 hcsvlab_robochef/paradisec/harvest/collection_harvester.py ${raw_path}/paradisec >> $logfile 2>&1
 bin/python2.7 main.py paradisec >> $logfile 2>&1
-
+if [ $? -ne 0 ]
+then
+  echo "Error manifesting PARADISEC. Check cron.log or paradisec_ingest.log"
+  exit 1
+fi
 cd $DIR/..
 bundle exec rake fedora:paradisec_clear
 
