00a318f36c0df5dbff3a2df2017a0683490b1736
Shaun Muscat <shaun@intersect.org.au>
2015-08-19 12:30:45 +1000

    HCSVLAB-996: refactor out functionality into helper methods

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -45,33 +45,14 @@ class CollectionsController < ApplicationController
   def create
     if request.format == 'json' and request.post?
       collection_name = params[:name]
-      if (!collection_name.nil? and !collection_name.blank? and !(collection_name.length > 255) and !(params[:collection_metadata].nil?))
-        # Parse JSON-LD formatted collection metadata and convert to RDF
-        json_md = params[:collection_metadata]
-        graph = RDF::Graph.new << JSON::LD::API.toRdf(json_md)
-        collection_rdf = graph.dump(:ttl, prefixes: {foaf: "http://xmlns.com/foaf/0.1/"})
-        collection_uri = graph.statements.first.subject.to_s
-        if !Collection.find_by_uri(collection_uri).present?  # ingest skips collections with non-unique name or uri
-          # write metadata and manifest file for ingest
-          corpus_dir = File.join(NEW_COLLECTION_DIR, collection_name)
-          metadata_file_path = File.join(NEW_COLLECTION_DIR,  collection_name + '.n3')
-          manifest_file_path = File.join(corpus_dir, MANIFEST_FILE_NAME)
-          FileUtils.mkdir_p(corpus_dir)
-          File.open(metadata_file_path, 'w') do |file|
-            file.puts collection_rdf
-          end
-          File.open(manifest_file_path, 'w') do |file|
-            manifest_hash = {"collection_name" => collection_name, "files" => {}}
-            file.puts(manifest_hash.to_json)
-          end
-
+      if !collection_name.nil? and !collection_name.blank? and !(collection_name.length > 255) and !(params[:collection_metadata].nil?)
+        collection_uri = get_uri_from_metadata(params[:collection_metadata])
+        if !Collection.find_by_uri(collection_uri).present?  # ingest skips collections with non-unique uri
+          corpus_dir = create_metadata_and_manifest(collection_name, convert_json_metadata_to_rdf(params[:collection_metadata]))
           ingest_corpus(corpus_dir)
           @success_message = "Request for new collection '#{collection_name}' (#{collection_uri}) sent to Administrator"
         else
-          err_message = "Collection '#{collection_name}' (#{collection_uri}) already exists in the system - skipping"
-          respond_to do |format|
-            format.any { render :json => {:error => err_message}.to_json, :status => 400 }
-          end
+          respond_with_error("Collection '#{collection_name}' (#{collection_uri}) already exists in the system - skipping", 400)
         end
       else
         invalid_name = collection_name.nil? or collection_name.blank? or collection_name.length > 255
@@ -80,15 +61,10 @@ class CollectionsController < ApplicationController
         err_message = "metadata parameter" if invalid_metadata
         err_message = "name and metadata parameters" if invalid_name and invalid_metadata
         err_message << " not found" if !err_message.nil?
-        respond_to do |format|
-          format.any { render :json => {:error => err_message}.to_json, :status => 400 }
-        end
+        respond_with_error(err_message, 400)
       end
     else
-      err_message = "JSON-LD formatted metadata must be sent to the add collection api call as a POST request"
-      respond_to do |format|
-        format.any { render :json => {:error => err_message}.to_json, :status => 404 }
-      end
+      respond_with_error("JSON-LD formatted metadata must be sent to the add collection api call as a POST request", 404)
     end
   end
 
@@ -175,5 +151,39 @@ class CollectionsController < ApplicationController
   #                           :last_page? => current_page < num_pages
   #  )
   #end
+
+  # Coverts JSON-LD formatted collection metadata and converts it to RDF
+  def convert_json_metadata_to_rdf(json_metadata)
+    graph = RDF::Graph.new << JSON::LD::API.toRdf(json_metadata)
+    graph.dump(:ttl, prefixes: {foaf: "http://xmlns.com/foaf/0.1/"})
+  end
+
+  # Gets the collection URI from JSON-LD formatted metadata
+  def get_uri_from_metadata(json_metadata)
+    graph = RDF::Graph.new << JSON::LD::API.toRdf(json_metadata)
+    graph.statements.first.subject.to_s
+  end
+
+  # Writes the collection manifest as JSON and the metadata as .n3 RDF
+  def create_metadata_and_manifest(collection_name, collection_rdf, collection_manifest={"collection_name" => collection_name, "files" => {}})
+    corpus_dir = File.join(NEW_COLLECTION_DIR, collection_name)
+    metadata_file_path = File.join(NEW_COLLECTION_DIR,  collection_name + '.n3')
+    manifest_file_path = File.join(corpus_dir, MANIFEST_FILE_NAME)
+    FileUtils.mkdir_p(corpus_dir)
+    File.open(metadata_file_path, 'w') do |file|
+      file.puts collection_rdf
+    end
+    File.open(manifest_file_path, 'w') do |file|
+      file.puts(collection_manifest.to_json)
+    end
+    corpus_dir
+  end
+
+  # Renders the given error message as JSON
+  def respond_with_error(message, status_code)
+    respond_to do |format|
+      format.any { render :json => {:error => message}.to_json, :status => status_code }
+    end
+  end
 
 end
