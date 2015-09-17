64ce0f095583979c8347c4f406d768043739dce7
Shaun Muscat <shaun@intersect.org.au>
2015-09-17 16:14:36 +1000

    HCSVLAB-1132: overwrite blacklight find method to use post request

--- lib/blacklight/blacklight_solrhelper_overrides.rb
+++ lib/blacklight/blacklight_solrhelper_overrides.rb
@@ -28,5 +28,60 @@ module Blacklight::SolrHelper::Overrides
     solr_response
   end
 
+  # # calls setup_previous_document then setup_next_document.
+  # # used in the show action for single view pagination.
+  # def setup_next_and_previous_documents
+  #   setup_previous_document
+  #   setup_next_document
+  # end
+  #
+  # def setup_previous_document
+  #   @previous_document = session[:search][:counter] ? setup_document_by_counter(session[:search][:counter].to_i - 1) : nil
+  # end
+  #
+  # def setup_next_document
+  #   @next_document = session[:search][:counter] ? setup_document_by_counter(session[:search][:counter].to_i + 1) : nil
+  # end
+  #
+  # # gets a document based on its position within a resultset
+  # def setup_document_by_counter(counter)
+  #   return if counter < 1 || session[:search].blank?
+  #   search = session[:search] || {}
+  #   get_single_doc_via_search(counter, search)
+  # end
+  #
+  # # a solr query method
+  # # this is used when selecting a search result: we have a query and a
+  # # position in the search results and possibly some facets
+  # # Pass in an index where 1 is the first document in the list, and
+  # # the Blacklight app-level request params that define the search.
+  # def get_single_doc_via_search(index, request_params)
+  #   solr_params = solr_search_params(request_params)
+  #   solr_params[:start] = (index - 1) # start at 0 to get 1st doc, 1 to get 2nd.
+  #   solr_params[:rows] = 1
+  #   solr_params[:fl] = '*'
+  #   solr_response = find(blacklight_config.qt, solr_params)
+  #   SolrDocument.new(solr_response.docs.first, solr_response) unless solr_response.docs.empty?
+  # end
+
+  # This overrides the BlackLight gems find method so that POST
+  # requests are used for queries instead of GETs, to avoid the problem
+  # where large GET queries are truncated and break Alveo.
+  def find(*args)
+    if Blacklight::version != '4.2.1'
+      raise 'This method modifies the default Blacklight::SolrHelper#query_solr method. ' \
+            'Update it everytime the gem is updated and remove it entirely for gem versions ' \
+            '5.0.0 and above, where the request method has become a configurable parameter.'
+    end
+
+    path = blacklight_config.solr_path
+    # response = blacklight_solr.get(path, :params=> args[1])
+    response = blacklight_solr.send_and_receive(path, :data=>args[1], :method => :post)
+    Blacklight::SolrResponse.new(force_to_utf8(response), args[1])
+  rescue Errno::ECONNREFUSED => e
+    raise Blacklight::Exceptions::ECONNREFUSED.new("Unable to connect to Solr instance using #{blacklight_solr.inspect}")
+  end
+
+
 
 end
