89a05f02d37af1dbfa28817a1528826efa5a328a
Shaun Muscat <shaun@intersect.org.au>
2015-11-11 16:22:59 +1100

    HCSVLAB-1122: move CSV import from rake to db seed

--- db/seed_helper.rb
+++ db/seed_helper.rb
@@ -5,4 +5,15 @@ def create_roles_and_permissions
   Role.create!(:name => Role::DATA_OWNER_ROLE)
 end
 
-
+# Populates the languages table with the language names and codes from the languages CSV file
+def populate_languages
+  Language.delete_all
+  languages = {}
+  csv_file = File.join('lib', 'resources', 'languages-2015-11-09.csv')
+  CSV.foreach(csv_file, :headers => true) do |csv_obj|
+    languages[csv_obj['Code']] = csv_obj['Name']
+  end
+  languages.each do |language_code, language_name|
+    Language.create!(:code => language_code, :name => language_name)
+  end
+end
