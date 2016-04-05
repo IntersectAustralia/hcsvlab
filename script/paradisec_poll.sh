24cfb6e129a0df1c033ea38bcafd14a28e2f64d2
Ilya Anisimoff <ilya@intersect.org.au>
2016-04-05 11:36:32 +1000

    HCSVLAB-MISC: Updates paradisec polling script

--- script/paradisec_poll.sh
+++ script/paradisec_poll.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
+# The following variables should be defined in .bashrc
+#
+# ROBOCHEF_PATH - path to robochef folder
+# RAW_PATH - path to store raw files
+# PROCESSED_PATH - path to store robochefed files
+# ROBOCHEF_VENV - path to the virtualenv activate script for robochef
+
+
 source $HOME/.bash_profile
+source $ROBOCHEF_VENV
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [ -z "$RAILS_ENV" ]; then
     RAILS_ENV=development
 fi
 
-robochef_path=/data/${RAILS_ENV}/paradisec_robochef
-raw_path=/data/${RAILS_ENV}/raw
-processed_path=/data/${RAILS_ENV}/processed
-
 echo "Rails env= $RAILS_ENV"
 
-if [ -d "$robochef_path" ]
+if [ -d "$ROBOCHEF_PATH" ]
 then
-    cd $robochef_path
+    cd $ROBOCHEF_PATH
     git pull
 else
-    mkdir -p $robochef_path
-    git clone https://github.com/IntersectAustralia/hcsvlab_robochef $robochef_path
-    cd $robochef_path
+    mkdir -p $ROBOCHEF_PATH
+    git clone https://github.com/IntersectAustralia/hcsvlab_robochef $ROBOCHEF_PATH
+    cd $ROBOCHEF_PATH
 fi
 
 function setup_error {
@@ -47,13 +52,13 @@ else
         sudo chmod -r a+w /usr/local/lib/python2.7
 
         setup_error $?
-        cd $robochef_path
+        cd $ROBOCHEF_PATH
     fi
 
-    cp $robochef_path/config.ini.dist $robochef_path/config.ini
-    echo "DOCUMENT_BASE_URL = file:///data/production_collections/" >> $robochef_path/config.ini
-    echo "CORPUS_BASEDIR = ${raw_path}/" >> $robochef_path/config.ini
-    echo "CORPUS_OUTPUTDIR = ${processed_path}/" >> $robochef_path/config.ini
+    cp $ROBOCHEF_PATH/config.ini.dist $ROBOCHEF_PATH/config.ini
+    echo "DOCUMENT_BASE_URL = file:///data/production_collections/" >> $ROBOCHEF_PATH/config.ini
+    echo "CORPUS_BASEDIR = ${RAW_PATH}/" >> $ROBOCHEF_PATH/config.ini
+    echo "CORPUS_OUTPUTDIR = ${PROCESSED_PATH}/" >> $ROBOCHEF_PATH/config.ini
 
     curl https://bootstrap.pypa.io/ez_setup.py -o - | python2.7
     setup_error $?
@@ -67,13 +72,13 @@ else
     setup_error $?
 fi
 
-logfile=$DIR/../log/paradisec_ingest.log
-echo "Logging to $logfile"
-echo "===== Begin $( date '+%Y-%m-%d %H.%M.%S' )" >> $logfile
-rm -r ${raw_path}/paradisec
-bin/python2.7 hcsvlab_robochef/paradisec/harvest/harvester.py ${raw_path}/paradisec >> $logfile 2>&1
-bin/python2.7 hcsvlab_robochef/paradisec/harvest/collection_harvester.py ${raw_path}/paradisec >> $logfile 2>&1
-bin/python2.7 main.py paradisec >> $logfile 2>&1
+LOGFILE=$DIR/../log/paradisec_ingest.log
+echo "Logging to $LOGFILE"
+echo "===== Begin $( date '+%Y-%m-%d %H.%M.%S' )" >> $LOGFILE
+rm -r ${RAW_PATH}/paradisec
+python hcsvlab_robochef/paradisec/harvest/harvester.py ${RAW_PATH}/paradisec >> $LOGFILE 2>&1
+python hcsvlab_robochef/paradisec/harvest/collection_harvester.py ${RAW_PATH}/paradisec >> $LOGFILE 2>&1
+python main.py paradisec >> $LOGFILE 2>&1
 if [ $? -ne 0 ]
 then
   echo "Error manifesting PARADISEC. Check cron.log or paradisec_ingest.log"
@@ -84,7 +89,7 @@ bundle exec rake fedora:paradisec_clear
 
 declare corpora
 
-for f in `find ${processed_path}/paradisec -type d| sort`
+for f in `find ${PROCESSED_PATH}/paradisec -type d| sort`
 do
    if test -n "$(find ${f} -maxdepth 1 -name '*-metadata.rdf' -print -quit)"
    then
@@ -93,5 +98,5 @@ do
    fi
 done
 
-./nohup_ingest_all_bg.sh ${corpora} >> $logfile 2>&1
-echo "===== End $( date '+%Y-%m-%d %H.%M.%S' )" >> $logfile
+./nohup_ingest_all_bg.sh ${corpora} >> $LOGFILE 2>&1
+echo "===== End $( date '+%Y-%m-%d %H.%M.%S' )" >> $LOGFILE
